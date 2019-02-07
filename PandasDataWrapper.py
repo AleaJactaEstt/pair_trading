c02e5c6f6b7347d06d0e9aee34c805ea267d7b02
m1grcl <arsenty.kambalin@gmail.com>
2019-02-07 19:36:12 +0300

    alpha version added

--- PandasDataWrapper.py
+++ PandasDataWrapper.py
@@ -9,16 +9,15 @@ class btPandasStrategy(bt.Strategy):
     '''
     class wrapper that help to process with pandas DataFrames
     '''
-    def __init__(self):
-        super().__init__()
-        
+    
     def get_df_slice(self, data, ago=0, size=0, datasname='close'):
         
-        assert (len(self) > size), "There are no data at that moment, try to get less size."
+        assert (len(self) >= size), "There are no data at that moment, try to get less size."
+
         data = data or self.datas[0]
-        
-        time_index = convert_btdate_to_dateIndex(ago=ago, lookback_window=size)
-        
+
+        time_index = self.convert_btdate_to_dateIndex(ago=ago, lookback_window=size)
+
         return pd.DataFrame(list(data.get(ago = ago, size = size)), index=time_index)
     
     def convert_btdate_to_dateIndex(self, ago = 0, lookback_window=0):
@@ -29,12 +28,15 @@ class btPandasStrategy(bt.Strategy):
         This method return pandas DateIndex from lookback_window to current time. 
         Special is that in bt another indexation 0 - is current time, -n - is n-th past time bar.
         '''
-        assert (len(self) > lookback_window), "There are no data at that moment, try to get less size."
-        
-        bt_date = [self.datetime.datetime(ago = i) for i in range(-lookback_window, ago+1, 1)]
-        
+		
+        assert (len(self) >= lookback_window), "There are no data at that moment, try to get less size."
+		
+        bt_date = [self.datetime.datetime(ago = i) for i in range(-lookback_window+1, ago+1, 1)]
+
         str_date = [str(date) for date in bt_date]
-        
+ 
         pd_dateIndex = pd.to_datetime(str_date, format="%Y-%m-%d %H:%M:%S")
-        
+ 
         return pd_dateIndex
+	
+	def get_all(self, data, ago=0, datasname='close')
